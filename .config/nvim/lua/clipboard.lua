8bac9e4474f91fd3096e9be01fb9ba5477fcf312
sstawkin <sstawkin@gmail.com>
2025-03-21 06:48:24 +0400

    update nvim

--- .config/nvim/lua/clipboard.lua
+++ .config/nvim/lua/clipboard.lua
@@ -1,13 +1,13 @@
 local M = {}
 local function should_add(event)
-    local length = #event.regcontents - 1
-    for _,line in ipairs(event.regcontents) do
-        length = length + #line
-        if length > 10000 then
-            return false
-        end
+  local length = #event.regcontents - 1
+  for _, line in ipairs(event.regcontents) do
+    length = length + #line
+    if length > 10000 then
+      return false
     end
-    return true
+  end
+  return true
 end
 M.handle_yank_post = function()
   local event = vim.v.event
@@ -15,7 +15,7 @@ M.handle_yank_post = function()
     -- vim.fn.chansend(vim.v.stderr, vim.fn.printf("\x1b]52;;%s\x1b\\", vim.fn.system("base64 | tr -d '\n'", join(, "\n"))))
     local joined = vim.fn.join(event.regcontents, '\n')
     local based = vim.fn.system("base64 | tr -d '\n'", joined)
-    vim.fn.chansend(vim.v.stderr, vim.fn.printf("\x1b]52;;%s\x1b\\", based))
+    vim.fn.chansend(vim.v.stderr, vim.fn.printf('\x1b]52;;%s\x1b\\', based))
   end
 end
 return M
